5a92bae8463fe60fdb037d3f87e1d69bc24139b8
Alek Novkovski <alek@greatcode2.com>
2023-01-07 20:49:59 +0100

    Add DELETE /delete code

--- working/server.js
+++ working/server.js
@@ -45,6 +45,22 @@ server.put('/update/:id', express.text({type: '*/*'}), (request,response)=>{
 
 //Add the /delete/:id code here!
 
+server.delete('/delete/:id', (request,response)=>{
+  var items = orderData.orders
+  var newData = {"orders": []}
+  items.forEach(function(o) {
+    console.log(o)
+    if (o.id == request.params.id){
+      console.log('Deleting order!')
+    } else{
+      newData.orders.push(o)
+    }
+  });
+
+  fs.writeFileSync('orders.json', JSON.stringify(newData));
+  response.send('Success');
+  console.log('Success');
+});
 
 server.listen(3000,()=>{
  console.log('Express server started at port 3000');
